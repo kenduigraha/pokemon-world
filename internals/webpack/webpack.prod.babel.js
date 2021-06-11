153dfca0ef442d4a9f4ab3c2a7b3da16e0427c18
Ken Duigraha Putra <ken@stockbit.com>
2021-06-11 15:36:57 +0700

    Update config PWA and path favicon image.

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -116,20 +116,20 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
+      name: 'Pokemon\'s World',
+      short_name: 'Pokemon\'s World',
+      description: 'Pokemon\s World is for you who like pokemon <3',
       background_color: '#fafafa',
       theme_color: '#b1624d',
       inject: true,
       ios: true,
       icons: [
         {
-          src: path.resolve('app/images/icon-512x512.png'),
+          src: path.resolve('app/images/pokemon-favicon.png'),
           sizes: [72, 96, 128, 144, 192, 384, 512],
         },
         {
-          src: path.resolve('app/images/icon-512x512.png'),
+          src: path.resolve('app/images/pokemon-favicon.png'),
           sizes: [120, 152, 167, 180],
           ios: true,
         },
